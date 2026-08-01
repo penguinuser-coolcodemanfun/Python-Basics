445d8376b0864da55c0430d864ec5ec6572bd16c
penguinuser-coolcodemanfun <tuxneel@gmail.com>
2026-08-01 12:11:09 +0530

    Introduction

---
+++ print.py
@@ -0,0 +1,13 @@
+# Printing my name
+print("My name is Neeldev")
+
+# Print any Integer
+
+print(5)
+
+# Print any decimal number
+
+print(5.15)
+
+# Welcome to the world of programming
+print("Welcome to the world of programming")
