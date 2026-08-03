77277fb7cbf5b1e6402dc394102af63c200708f8
penguinuser-coolcodemanfun <tuxneel@gmail.com>
2026-08-03 12:06:51 +0530

    Variables

---
+++ Welcome.py
@@ -0,0 +1,18 @@
+# Print a Sentence
+print("Welcome to the world of programming")
+
+
+
+# Print a Number
+print(4)
+
+
+# Next line after print
+print("Hello Python \n ")
+
+
+# Pass multiple arguments for printing
+print("Hello", 5)
+
+#End arguments for printing
+print("welcome to ", end="*")
---
+++ variables.py
@@ -0,0 +1,16 @@
+# Creating diffrent veriables
+
+x = 5 
+y = "John"
+
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+
+# User Input
+name = input("enter your name: ")
+
+print("\n Hello", name , "\n welcome to codingal")
