f9e64c494cb424bae032251cff710ffe972d1668
penguinuser-coolcodemanfun <tuxneel@gmail.com>
2026-08-03 11:33:41 +0530

    Intro Acp

---
+++ acp.py
@@ -0,0 +1,11 @@
+# Introdution
+print("In the morning, I wake up at 6 am and brush my teeth")
+print("Then I eat food")
+print("After that i get dressed to go to School at 7 am")
+# In school
+print("In school i learn new things")
+print("When I'm home, I do my homework at 5 pm and then I go for a walk or do cycling at 6 pm")
+print("Then in the evening i study for 2 hrs")
+# Thank you
+print("And in a week the total study time is")
+print(2 * 7, "hrs")
